faf0d98e13ac8131883de2a6c93a7a2c99e088ad
tunsmm <dragon553180@gmail.com>
2022-12-26 23:45:25 +0300

    Global changing: adapting new format of history storing. Add get_user_history to get user with history and change get_user to get user without history. Add typing hints.

--- yourmeals/controllers/main_controller.py
+++ yourmeals/controllers/main_controller.py
@@ -1,3 +1,5 @@
+import datetime
+
 from yourmeals.data_access.data_access_module import DataAccessModule as DAM
 from yourmeals.models.meal import get_meal
 from yourmeals.controllers.recommendation_controller import RecommendationController
@@ -41,32 +43,39 @@ class MainController(metaclass=Singleton):
         user.gender = gender
         self.dam.update_user(user)
     
-    def add_meal_to_user(self, email: str, meal_type: str, date, dishes: list[str] = None, ):
-        user = self.dam.get_user(email=email)
+    def add_meal_to_user(self, email: str, meal_type: str, date: datetime.datetime, ):
+        user = self.dam.get_user(email=email, add_history=True)
         meal = get_meal(meal_type)
-        meal.date = date
-        # for str_dish in dishes:
-        #     meal.add_dish(str_dish)
+        meal.date = date.date()
+        meal.time = date.time()
         user.add_meal(meal)
         self.dam.update_user(user)
         
-    def delete_meal(self, email: str, date):
-        user = self.dam.get_user(email=email)
-        user.delete_meal(date)
+    def delete_meal(self, email: str, date: dict, time: dict):
+        user = self.dam.get_user(email=email, add_history=True)
+        user.delete_meal(
+            utils.dict_to_date(date), 
+            utils.dict_to_time(time)
+        )
         self.dam.update_user(user)
         
-    def add_dish_to_meal(self, email: str, date, dishes: list[str]):
-        user = self.dam.get_user(email=email)
-        meal = user.get_meal(date)
-        for str_dish in dishes:
-            dish = self.dam.get_dish(dish_name=str_dish)
+    def add_dish_to_meal(self, email: str, date: dict, time: dict, dishes: list[str]):
+        user = self.dam.get_user(email=email, add_history=True)
+        date = utils.dict_to_date(date)
+        time = utils.dict_to_time(time)
+        meal = user.get_meal(date, time)
+        for dish_name in dishes:
+            dish = self.dam.get_dish(dish_name=dish_name)
             meal.add_dish(dish)
-        user.set_meal(date, meal)
+        user.set_meal(date, time, meal)
         self.dam.update_user(user)
 
-    def delete_dish_on_meal(self, email: str, date, dish_name: str):
-        user = self.dam.get_user(email=email)
-        meal = user.get_meal(date)
+    def delete_dish_on_meal(self, email: str, date: dict, time: dict, dish_name: str):
+        user = self.dam.get_user(email=email, add_history=True)
+        meal = user.get_meal(
+            utils.dict_to_date(date),
+            utils.dict_to_time(time)
+        )
         meal.delete_dish(dish_name)
         self.dam.update_user(user)
     
@@ -80,6 +89,10 @@ class MainController(metaclass=Singleton):
     
     def get_user(self, email: str) -> str:
         user = self.dam.get_user(email=email)
+        return utils.toJSON(user)
+    
+    def get_user_history(self, email: str) -> str:
+        user = self.dam.get_user(email=email, add_history=True)
         return utils.toJSON(user)
     
     def get_full_meals_recommendation(self, email: str) -> str:
