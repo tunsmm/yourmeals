9cd56bf8909de6b972f5a0130f0df58c9085ad4a
tunsmm <dragon553180@gmail.com>
2023-01-04 23:45:30 +0300

    Add constraints to login form

--- yourmeals/diaryapp/forms.py
+++ yourmeals/diaryapp/forms.py
@@ -6,9 +6,9 @@ from .models import STRATEGY_CHOICES, GENDER_CHOICES, MEAL_TYPE_CHOICES
 class UserForm(forms.Form):
     email = forms.EmailField(help_text='yourmail@mail.ru', label=('Электронная почта'), )
     name = forms.CharField(help_text='Иван Иванов', label=('Имя'), )
-    age = forms.IntegerField(label=('Возраст'),)
-    weight = forms.FloatField(label=('Вес'),)
-    height = forms.FloatField(label=('Рост'),)
+    age = forms.IntegerField(label=('Возраст'), min_value=1, max_value=200)
+    weight = forms.FloatField(label=('Вес'), min_value=1, max_value=500)
+    height = forms.FloatField(label=('Рост'), min_value=1, max_value=300)
     strategy = forms.ChoiceField(choices=STRATEGY_CHOICES, label=('Цель диеты'),)
     gender = forms.ChoiceField(choices=GENDER_CHOICES, label=('Пол'),)
 
@@ -19,4 +19,4 @@ class MealForm(forms.Form):
 
 
 class LoginForm(forms.Form):
-    email = forms.EmailField(label='Ваша почта', required=True)
+    email = forms.EmailField(help_text='yourmail@mail.ru', label='Ваша почта', required=True)
