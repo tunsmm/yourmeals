bf6fc41702aac474f41f606ae5762fc1aa3e4308
tunsmm <dragon553180@gmail.com>
2023-04-12 19:50:28 +0300

    Add custom exceptions

---
+++ yourmeals/exceptions/does_not_exist.py
@@ -0,0 +1,17 @@
+class DoesNotExistError(Exception):
+ 
+    # Constructor or Initializer
+    def __init__(self, value):
+        self.value = value
+ 
+    # __str__ is to print() the value
+    def __str__(self):
+        return(repr(self.value))
+
+
+class UserDoesNotExistError(DoesNotExistError):
+    ...
+
+
+class DishDoesNotExistError(DoesNotExistError):
+    ...
