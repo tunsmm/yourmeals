ea357df59a72cf65caaa4f6b69530c81da581272
tunsmm <dragon553180@gmail.com>
2023-05-19 22:14:18 +0300

    Change user history reading

--- yourmeals/recommendation/naive_recommender.py
+++ yourmeals/recommendation/naive_recommender.py
@@ -1,3 +1,4 @@
+from itertools import chain
 from operator import attrgetter
 
 from yourmeals.data_access.data_access_module import DataAccessModule as DAM
@@ -8,11 +9,24 @@ class NaiveRecommender:
         self.dam = DAM()
 
     def get_recommendation(self, user_email):
-        user = self.dam.get_user(user_email)
+        user = self.dam.get_user(user_email, add_history=True)
         history = user.history
-        meals = list(filter(lambda meal: len(meal.dishes) > 0, history))
-        if len(history) <= 1:
+        if not history:
             return None
-        last_meal = max(meals, key=attrgetter('date'))
-        dish = max(last_meal.dishes, key=attrgetter('calories'))
+        
+        last_meals = list()
+        for meals in history.values():
+            last_meals.extend(meals[::-1])
+            if any([bool(meal.dishes) for meal in meals[::-1]]):
+                break
+        
+        last_dishes = list()
+        for meal in last_meals:
+            if meal.dishes:
+                last_dishes.extend(meal.dishes[::-1])
+                break
+        if not last_dishes:
+            return None
+        
+        dish = max(last_dishes, key=attrgetter('calories'))
         return dish.name
